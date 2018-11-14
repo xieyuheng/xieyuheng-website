bbc56a724e25ab1315ef85f96225dbcba167aa52
Xie Yuheng <xyheme@gmail.com>
2018-11-14 11:32:18 +0800

    up

--- demo/cicada-notebook/main.js
+++ demo/cicada-notebook/main.js
@@ -190,7 +190,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./cicada_notebook_bg.wasm":"700c614a8945b71fb071"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./cicada_notebook_bg.wasm":"d8b2ff9a4d3373aaa76e"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
