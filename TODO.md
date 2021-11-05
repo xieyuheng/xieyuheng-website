b583d1e2ebd72d2fddb8839fcaa7757a8b66385d
Xie Yuheng <xyheme@gmail.com>
2021-11-05 17:43:00 +0800

    - setup prettier

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-- setup prettier
 - setup TailwindCSS
 - setup `types/`
 - setup `views/routes.ts`
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator"
+import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
 
 @Component({
   components: {
