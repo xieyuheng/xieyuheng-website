478efa35e1d81d5d0f1b2f981a44c49e6d9ddfe1
Xie Yuheng <xyheme@gmail.com>
2021-11-07 19:45:08 +0800

    up

--- src/views/notes/routes.ts
+++ src/views/notes/routes.ts
@@ -2,4 +2,21 @@ import { RouteConfig } from "vue-router"
 
 export const routes: Array<RouteConfig> = [
   { path: "/notes", component: () => import("@/views/notes/note-list.vue") },
+  // {
+  //   path: "/notes/*",
+  //   component: () => import("@/views/notes/note-layout.vue"),
+  //   props: (route) => ({
+  //     articleId: route.params.pathMatch,
+  //   }),
+  //   children: [
+  //     {
+  //       path: "",
+  //       component: () => import("@/views/notes/note.vue"),
+  //       props: (route) => ({
+  //         noteId: route.params.pathMatch,
+  //         baseURL: `${window.location.origin}/articles`,
+  //       }),
+  //     },
+  //   ],
+  // },
 ]
