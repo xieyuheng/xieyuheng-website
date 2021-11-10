17773a12ff641db7752a9b6bc82fdeea15d5d530
Xie Yuheng <xyheme@gmail.com>
2021-11-10 22:29:46 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue"
 import router from "./router"
 import "./styles/tailwind.css"
 import "./assets/fonts/fonts.css"
-// import "./plugins/register-service-worker"
+import "./plugins/register-service-worker"
 import "./plugins/highlight"
 import "./app"
 
