b28d5d3fb9b9ec500ccd631ac5892c0d6c38393a
Xie Yuheng <xyheme@gmail.com>
2021-12-24 04:32:22 +0800

    update vue/cli to update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require("tailwindcss/colors")
-
 module.exports = {
   purge: [
     "./src/**/*.vue",
@@ -9,43 +7,8 @@ module.exports = {
     fontFamily: {
       sans: ["Linux Biolinum O", "sans-serif"],
       serif: ["Linux Libertine O", "serif"],
-      narration: [
-        "EBGaramond",
-        // "BodoniModa",
-        "serif",
-      ],
-      mono: [
-        // "Inconsolata LGC",
-        // "Terminus (TTF)",
-        "Fira Code",
-        // "DejaVu Sans Mono",
-        "monospace",
-      ],
+      mono: ["Fira Code", "monospace"],
     },
-    extend: {
-      screens: {
-        "2md": "880px",
-      },
-      colors: {
-        gray: colors.warmGray,
-        orange: colors.orange,
-        amber: colors.amber,
-        lime: colors.lime,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        sky: colors.sky,
-        violet: colors.violet,
-        fuchsia: colors.fuchsia,
-        rose: colors.rose,
-      },
-    },
-  },
-  variants: {
-    extend: {
-      opacity: ["disabled"],
-      backgroundColor: ["disabled"],
-      borderWidth: ["hover", "focus"],
-    },
+    extend: { screens: { "2md": "880px" } },
   },
 }
