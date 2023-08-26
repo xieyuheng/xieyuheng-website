18023e462416c67781e98763db8ca2c496e789d6
Xie Yuheng <xyheme@gmail.com>
2023-08-27 04:04:46 +0800

    simple font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,5 @@
 import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
 
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      rotate: {
-        20: '20deg',
-      },
-      listStyleType: {
-        square: 'square',
-      },
-    },
-    fontFamily: {
-      sans: ['Linux Biolinum O', 'Source Han Serif SC', 'sans-serif'],
-      serif: ['Linux Libertine O', 'LXGW WenKai', 'serif'],
-      // NOTE One Chinese character should equal to two English characters.
-      mono: ['Fira Code', 'LXGW WenKai Larger', 'monospace'],
-      logo: ['Bodoni Moda', 'Source Han Serif SC', 'serif'],
-    },
-  },
 } satisfies Config
