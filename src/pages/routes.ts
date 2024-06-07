60249d4b812261ceff303a5758ed8c074ba7176a
Xie Yuheng <xyheme@gmail.com>
2024-06-07 21:00:55 +0800

    `errors/` -- use tsx

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -1,6 +1,9 @@
 import type { RouteRecordRaw } from 'vue-router'
-import PageNotFound from './errors/PageNotFound.vue'
+import { PageNotFound } from './errors/PageNotFound.tsx'
 import { Home } from './home/Home.tsx'
+
+;(Home as any).displayName = 'Home'
+;(PageNotFound as any).displayName = 'PageNotFound'
 
 export const routes: Array<RouteRecordRaw> = [
   { path: '/', component: Home },
