9706a1e3889c89102b0e93fb01757caa3c510592
Xie Yuheng <xyheme@gmail.com>
2024-06-07 21:06:56 +0800

    up

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -2,6 +2,10 @@ import type { RouteRecordRaw } from 'vue-router'
 import { PageNotFound } from './errors/PageNotFound.tsx'
 import { Home } from './home/Home.tsx'
 
+// NOTE To use 'vue-router', we have to
+// set `displayName` for all components.
+// - https://github.com/vuejs/router/pull/922/files
+
 ;(Home as any).displayName = 'Home'
 ;(PageNotFound as any).displayName = 'PageNotFound'
 
