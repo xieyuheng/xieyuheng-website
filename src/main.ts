68434c8ad72f3ab330de9639c7690b5d24557837
Xie Yuheng <xyheme@gmail.com>
2024-06-12 20:42:49 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { happyHacking } from './console/happyHacking'
 import router from './router'
-import './styles/fonts.css'
 import './styles/index.css'
 
 const app = createApp(App)
