705549cc138cb229f8b42d964d196705936b7a84
Xie Yuheng <xyheme@gmail.com>
2024-06-12 20:45:33 +0800

    up

--- src/console/happyHacking.ts
+++ src/console/happyHacking.ts
@@ -1,6 +1,7 @@
 export function happyHacking(): void {
   console.group('From Xie Yuheng:')
   console.log(`Welcome to my homepage :)`)
+  console.log(`Last edited: 2024-06-12`)
   console.log(`This project is open source.`)
   console.log(
     `The source code is at https://github.com/xieyuheng/xieyuheng-website`,
