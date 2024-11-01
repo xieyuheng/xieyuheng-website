0a4b8589b5489a500059d8de82b51d4089bf1c3e
Xie Yuheng <xyheme@gmail.com>
2024-11-01 16:19:07 +0800

    remove happyHacking

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createHead } from '@vueuse/head'
 import { createApp } from 'vue'
 import App from './App.vue'
-import { happyHacking } from './console/happyHacking'
 import router from './router'
 import './styles/index.css'
 
@@ -11,5 +10,3 @@ app.use(router)
 app.use(createHead())
 
 app.mount('#app')
-
-happyHacking()
