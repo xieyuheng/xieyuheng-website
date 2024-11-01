2020e162d3bc255b24cbb649c02ba8ac96f8c801
Xie Yuheng <xyheme@gmail.com>
2024-11-01 16:21:58 +0800

    up

--- src/utils/pwa/hasServiceWorker.ts
+++
@@ -1,5 +0,0 @@
-export async function hasServiceWorker() {
-  const registrations = await navigator.serviceWorker.getRegistrations()
-
-  return registrations.length > 0
-}
--- src/utils/pwa/removeServiceWorker.ts
+++
@@ -1,7 +0,0 @@
-export async function removeServiceWorker() {
-  const registrations = await navigator.serviceWorker.getRegistrations()
-
-  for (const registration of registrations) {
-    registration.unregister()
-  }
-}
