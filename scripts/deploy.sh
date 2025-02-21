2d01355fcf67411151846a7c88c3ed5678fad59d
Xie Yuheng <xyheme@gmail.com>
2025-02-22 04:39:12 +0800

    up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-git pull && npm i && npm run build && sudo rm -rf /websites/www && sudo cp -r dist /websites/www
+git pull && npm i && npm run build && sudo rm -rf /websites/xieyuheng && sudo cp -r dist /websites/xieyuheng
